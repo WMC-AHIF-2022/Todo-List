ddd6e3a6d3387eebb68940c027cc99a4250e1251
elplak <elplak19@gmail.com>
2023-06-23 11:53:21 +0200

    fixed untracked files

--- src/public/scripts/login-signup-logout/logout.js
+++
@@ -1,8 +0,0 @@
-function logout() {
-    sessionStorage.clear();
-    window.location.href = "../../login-signup.html";
-}
-
-window.addEventListener("load", () => {
-    document.querySelector(".dropdown-btn").innerHTML = sessionStorage.getItem("username");
-})
